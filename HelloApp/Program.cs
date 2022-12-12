f837fe9a6bfb301038b7f98246dc2949f23ef70f
prakashsaud123 <prakashsaud488@gmail.com>
2022-12-12 19:38:01 +0545

    add new text

---
+++ HelloApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("hello\n hello\n hello"); //Escape sequences:\n \t \b
+
+String name = Console.ReadLine();
+Console.WriteLine(name);
