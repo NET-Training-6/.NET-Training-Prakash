f98dedd4e45fe216c4ff9823fda9e1b2a263b258
prakashsaud123 <prakashsaud488@gmail.com>
2022-12-18 16:24:56 +0545

    Learn About Datatypes

--- HelloApp/Entry.cs
+++ HelloApp/Entry.cs
@@ -2,6 +2,56 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Hello");
-    }
-}
+        // // Numbers: integers and floating point
+        // // Integers
+        // byte a = 255; // Unsigned - positive values only 0-255
+        // short b = -2323;
+        // int c = -4242424;
+        // long d = 3223242425;
+
+        // // Floating points
+        // float e = 345.6742342423424242342f;
+        // double f = 355335.445453790823798027498;
+        // decimal g = 35324526.352344242423245243523525252m;
+
+        // // Text
+        // string name = "Prakash Saud";
+        // char gender = 'M';
+
+        // // Boolean
+        // bool areYouMale = true;
+        // bool? sex = null; // true => Male, false => Female, null => Others
+
+        //         Console.WriteLine("Learn About Variables");
+        //         Console.WriteLine(a);
+        //         Console.WriteLine(b);
+        //         Console.WriteLine(c);
+        //         Console.WriteLine(d);
+        //         Console.WriteLine(e);
+        //         Console.WriteLine(f);
+        //         Console.WriteLine(g);
+        //         Console.WriteLine(name);
+        //         Console.WriteLine(gender);
+        //         Console.WriteLine(areYouMale);
+
+
+
+        //     }
+        // }
+
+
+        var number = 80;
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine(number + " is even");
+        }
+        else
+        {
+            Console.WriteLine(number + " is odd");
+        }
+        }
+        }
+   
+
+
