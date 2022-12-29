9e472f58c665f48645d11268708b341ffc72dec6
prakashsaud123 <prakashsaud488@gmail.com>
2022-12-29 13:02:00 +0545

    Learn About Loops

---
+++ HelloApp/1C-Arrays.cs
@@ -0,0 +1,25 @@
+class ArrayLeaner
+{
+   public void LearnArrays()
+    {
+        // One dimensional arrays
+        byte[] ages = { 56, 89, 50, 20, 13 };
+        var x = ages[4];
+        var l = ages.Length;
+        Console.WriteLine(l);
+        var numbers = new int[100];
+        
+        // Multi dimensional arrays
+        var matrix = new double [22, 10];
+        // 45   89
+        // 33   55
+
+
+
+
+        
+    }
+}
+// Create appropriate data structure for following:
+// 1. Hold names of 10 people and print  them  to Standard console.
+// 2. Store salaries of 6 people and  
--- HelloApp/Entry.cs
+++ HelloApp/Entry.cs
@@ -2,94 +2,13 @@
 {
     public static void Main()
     {
-        // // Numbers: integers and floating point
-        // // Integers
-        // byte a = 255; // Unsigned - positive values only 0-255
-        // short b = -2323;
-        // int c = -4242424;
-        // long d = 3223242425;
-
-        // // Floating points
-        // float e = 345.6742342423424242342f;
-        // double f = 355335.445453790823798027498;
-        // decimal g = 35324526.352344242423245243523525252m;
-
-        // // Text
-        // string name = "Prakash Saud";
-        // char gender = 'M';
-
-        // // Boolean
-        // bool areYouMale = true;
-        // bool? sex = null; // true => Male, false => Female, null => Others
-
-        //         Console.WriteLine("Learn About Variables");
-        //         Console.WriteLine(a);
-        //         Console.WriteLine(b);
-        //         Console.WriteLine(c);
-        //         Console.WriteLine(d);
-        //         Console.WriteLine(e);
-        //         Console.WriteLine(f);
-        //         Console.WriteLine(g);
-        //         Console.WriteLine(name);
-        //         Console.WriteLine(gender);
-        //         Console.WriteLine(areYouMale);
-
-
-
-        //     }
-        // }
-
-        //         var input = Console.ReadLine();
-        //         var number = int.Parse(input);
-
-        //         if (number % 2 == 0)
-        //         {
-        //             Console.WriteLine(number + " is even");
-        //         }
-        //         else
-        //         {
-        //             Console.WriteLine(number + " is odd");
-
-        // Cw - Find simple interest for principal amount, time and rate given by user.
-
-        //     Console.WriteLine("principal amount");
-        //     int amount = int.Parse(Console.ReadLine());
-        //     Console.WriteLine("Enter a time period");
-        //     int time_period = int.Parse(Console.ReadLine());
-        //     Console.WriteLine("interest rate");
-        //     int interestrate = int.Parse(Console.ReadLine());
-        //     int simple_interest = amount * time_period * interestrate / 100;
-        //     Console.WriteLine("The simple interest =" + simple_interest);
-        //     Console.ReadLine();
-        // }
-        // }
-
-        //Find if user supplied number is multiple of 3 and 5
-
-
-        Console.Write("Enter an number:");
-        int x = Convert.ToInt32(Console.ReadLine());
-        if ((x % 3 == 0) && (x % 5 == 0))
-        {
-            Console.WriteLine("The number" + x + " is multiple of 3 and 5");
-        }
-        else
-        {
-            Console.WriteLine("The number" + x + " is not  multiple of 3 and 5");
-        }
+        LearnLoops p1 = new LearnLoops();
+        p1.IncPattern();
+        p1.DecPattern();
+        p1.HashPattern();
     }
+
 }
-
-
-
-
-
-
-//             Console.ReadKey();
-
-//         }
-//     }
-// }
 
 
 
